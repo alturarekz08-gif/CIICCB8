18c3b3b3e5610e0a885abd2627d67092ef43024f
rekzaltura <alturarekz08@gmail.com>
2025-10-27 07:52:03 +0000

    day_13.2

---
+++ chapter2/Operators.java
@@ -0,0 +1,17 @@
+package chapter2;
+
+public class Operators {
+    public static void main(String[] args) {
+       System.out.println(15>>>1);
+        System.out.println(2<<2);
+        // 2 shorts = int
+        //if what is the higher number will be promoted   
+        System.out.println(+2+ +62);
+        int a = 0;
+        System.out.println(a++);
+        System.out.println(a++ - ++a);
+        System.out.println(a);
+         
+    }
+    
+}

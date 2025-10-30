0965f52ba9a9f72ae1ca6b9cdda11c3cb638113f
rekzaltura <alturarekz08@gmail.com>
2025-10-30 05:21:44 +0000

    task3

---
+++ chapter2/Task3.java
@@ -0,0 +1,20 @@
+package chapter2;
+
+class Task3 {
+
+    public static void main(String[] args) {
+        String a = "Wow";
+        String b = a;
+        String c = "Different";
+        String d = c + "!";
+
+        boolean b1 = a == b;
+        boolean b2 = d.equals(d + "!");
+        boolean b3 = !c.equals(a);
+
+        if(b1 && b2 && b3);
+            System.out.println("succcess");
+
+
+    }
+}
